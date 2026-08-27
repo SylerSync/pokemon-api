8a56846dafbe1cc49f1c4ab36ea0f233b2e9c49b
brendan-sync <brendan.klostermann@syncbak.com>
2026-08-27 09:18:00 -0500

    Created DataObjects for Items
    Created the DTO for Item
    Created base IItemRepository

---
+++ Core/Domain/DataObjects/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Domain.DataObjects
+{
+    public abstract class Item
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Category { get; private set; }
+        public int Cost { get; private set; }
+
+        protected Item(string id, string name, string category, int cost)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+            Cost = cost;
+        }
+
+    }
+
+
+}
---
+++ Core/Domain/DataObjects/MegaEvolutionItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Domain.DataObjects
+{
+    public class MegaEvolutionItem : Item
+    {
+
+        public string PokemonName { get; private set; }
+        public string MegaFormName { get; private set; }
+        public string Description { get; private set; }
+
+        public MegaEvolutionItem(string id, string name, string category, int cost, string pokemonName, string megaName, string description) : base(id, name, category, cost)
+        {
+            PokemonName = pokemonName;
+            MegaFormName = megaName;
+            Description = description;
+        }
+    }
+}
