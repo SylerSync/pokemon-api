07580469fc165ef0508547a8299fcf169fc1d1ea
brendan-sync <brendan.klostermann@syncbak.com>
2026-08-27 15:49:56 -0500

    Removed all seeder functions

--- controllers/Program.cs
+++ controllers/Program.cs
@@ -1,9 +1,9 @@
 using Core.Domain.Repositories.Abstactions;
-using Core.Infrastructure.MongoSeedData;
 using Core.Infrastructure.Repositories;
 using Core.Services;
 using Core.Services.Abstractions;
 using MongoDB.Driver;
+using System.Diagnostics;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,16 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-string connectionString = builder.Configuration.GetConnectionString("MongoDb") ?? "mongodb://localhost:27018";
-string databaseName = builder.Configuration["DatabaseSettings:DatabaseName"] ?? "PokemonDb";
-
-builder.Services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
-builder.Services.AddScoped<IMongoDatabase>(sp =>
-{
-    var client = sp.GetRequiredService<IMongoClient>();
-    return client.GetDatabase(databaseName);
-});
 
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -34,24 +24,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
-    using (var scope = app.Services.CreateScope())
-    {
-        try
-        {
-            var database = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
-            var seeder = new MongoDataSeeder(database);
-
-            // Execute seeder synchronously for startup
-            Task.Run(async () => await seeder.SeedAllAsync()).Wait();
-
-            Console.WriteLine("[Startup] Database seeding completed successfully.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"[Startup] Error during database seeding: {ex.Message}");
-        }
-    }
 }
 
 app.UseHttpsRedirection();
